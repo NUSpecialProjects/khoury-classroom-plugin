43d621a8a110d258b91ff7a567980e36a5a1e66f
kennybc <54950614+kennybc@users.noreply.github.com>
2024-10-07 16:45:45 -0400

    button component + assignment details head

--- frontend/src/pages/Assignments/AssignmentDetails/index.tsx
+++ frontend/src/pages/Assignments/AssignmentDetails/index.tsx
@@ -1,8 +1,26 @@
+import { FaChevronLeft } from "react-icons/fa";
+
+import "./styles.css";
+import Button from "@/components/Button";
+
 const AssignmentDetails: React.FC = () => {
   return (
     <div className="AssignmentDetails">
       <div className="AssignmentDetails__head">
-        <span>Assignment 3</span>
+        <div className="AssignmentDetails__title">
+          <FaChevronLeft />
+          <span>Assignment 3</span>
+        </div>
+        <div className="AssignmentDetails__dates">
+          <span>Released: 5 Sep, 10:00am</span>
+          <span>Due: 15 Sep, 11:59pm</span>
+        </div>
+      </div>
+
+      <div className="AssignmentDetails__externalButtons">
+        <Button href="">View in Github Classroom</Button>
+        <Button href="">View Starter Code</Button>
+        <Button href="">View Rubric</Button>
       </div>
     </div>
   );
