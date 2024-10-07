0cb1014f8d727c47d4081da666da5d89c65f0bde
kennybc <54950614+kennybc@users.noreply.github.com>
2024-10-07 15:46:16 -0400

    reformat pages index, add initial routing

---
+++ frontend/src/pages/Assignments/AssignmentDetails/index.tsx
@@ -0,0 +1,11 @@
+const AssignmentDetails: React.FC = () => {
+  return (
+    <div className="AssignmentDetails">
+      <div className="AssignmentDetails__head">
+        <span>Assignment 3</span>
+      </div>
+    </div>
+  );
+};
+
+export default AssignmentDetails;
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Settings from "./Settings";
 import Dashboard from "./Dashboard";
 import Assignments from "./Assignments";
+import AssignmentDetails from "./Assignments/AssignmentDetails";
 import Grading from "./Grading";
 
-
-export {Settings, Dashboard, Assignments, Grading}
+export { Settings, Dashboard, Assignments, AssignmentDetails, Grading };
