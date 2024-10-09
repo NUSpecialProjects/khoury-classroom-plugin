203dea8d2f4d86624d8aaa377b6864cae4d27cc4
Nicholas Tietje <tietje.n@northeastern.edu>
2024-10-09 14:50:20 -0400

    remove unused claims struct

--- backend/internal/models/claims.go
+++
@@ -1,9 +0,0 @@
-package models
-
-import "github.com/golang-jwt/jwt/v5"
-
-type Claims struct {
-	User  map[string]interface{} `json:"user"`
-	Token string                 `json:"token"`
-	jwt.RegisteredClaims
-}
