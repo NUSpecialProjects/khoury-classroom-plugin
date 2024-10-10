277a0fc832a96e8eb6f738d04ad560405394f52c
Alex Angione <111721125+alexangione419@users.noreply.github.com>
2024-10-10 11:34:19 -0400

    classrooms page

--- frontend/src/pages/Classrooms/index.tsx
+++ frontend/src/pages/Classrooms/index.tsx
@@ -1,19 +1,82 @@
 import TopNav from "@/components/Layout/TopNav";
 
+import "./styles.css";
+import { useEffect, useState } from "react";
+
+type Classroom = {
+    id: number;
+    name: string;
+    archived: string;
+    url: string;
+}
+
+
 const Classrooms: React.FC = () => {
-    
-    return(
+    const [classrooms, setClassrooms] = useState<Classroom[]>([]);
+
+    useEffect(() => {
+        // Replace with your API endpoint
+        // fetch('https://api.example.com/items')
+        //   .then(response => {
+        //     if (!response.ok) {
+        //       throw new Error('Network response was not ok');
+        //     }
+        //     return response.json();
+        //   })
+        //   .then(data => {
+        //     setClassrooms(data);
+        //   })
+        //   .catch(error => {
+        //     console.error('Error fetching data:', error);
+        //   });
+        //Fake data go brrrrrr
+
+        const c: Classroom[] = [
+            // {
+            //     id: 13232,
+            //     name: "CS3200 Database Desgin",
+            //     archived: ", false",
+            //     url: "yes.com"
+            // },
+            // {
+            //     id: 13232,
+            //     name: "CS3200 Database Desgin",
+            //     archived: ", false",
+            //     url: "yes.com"
+            // }
+        ]
+        setClassrooms(c)
+    }, []);
+
+    return (
         <div className="Classrooms">
             <div>
-                <TopNav/>
+                <TopNav />
             </div>
             <div className="Classrooms__bottom">
                 <div className="Classrooms__content">
                     <div className="Classrooms__title">Classrooms</div>
+                    <div className="Classrooms__list">
+                        {classrooms.length === 0 ? (
+                            <div className="Classrooms__none"> No Classrooms Found...</div>
+                        ) : (
+                            <div>
+                                {classrooms.map(item => (
+                                <div className="Classrooms__classLink">
+                                    <div>{item.name}</div>
+                                    <div className="Classrooms__linkProf">PRofessor</div>
+                                </div>
+                                ))}
+                            </div>
+                            
+                        )}
+                        
+                    </div>
+
                 </div>
             </div>
         </div>
     )
-} 
+}
 
 export default Classrooms;
