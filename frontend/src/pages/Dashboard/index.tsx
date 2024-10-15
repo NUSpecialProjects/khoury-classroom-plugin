54ade4d916108ebc06c7dbaa9f6fa0d0e2fae5ac
nandini-ghosh <nandini.ghosh2094@gmail.com>
2024-10-15 13:32:22 -0400

    table component

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -1,5 +1,6 @@
 import "./styles.css";
 import UserGroupCard from "@/components/UserGroupCard";
+import { Table, TableRow, TableCell } from "@/components/Table/index.tsx";
 
 const Dashboard: React.FC = () => {
     return (
@@ -10,38 +11,37 @@ const Dashboard: React.FC = () => {
                 <UserGroupCard label="Students" number={38} />
             </div>
             <div className="Dashboard__assignmentsWrapper">
-                <div className="Dashboard__assignmentsHeader">
-                    Active Assignments (1)</div>
-                <div className="Dashboard__assignments">
-                    <div className="Dashboard__assignment">
-                        <div>Assignment Name</div>
-                        <div>Released</div>
-                        <div>Due Date</div>
-                    </div>
+            <h2 style={{ marginBottom: 0 }}>Active Assignments</h2>
+                <Table cols={3}>
+                    <TableRow style={{ borderTop: "none" }}>
+                        <TableCell>Assignment Name</TableCell>
+                        <TableCell>Released</TableCell>
+                        <TableCell>Due Date</TableCell>
+                    </TableRow>
                     {Array.from({ length: 1 }).map((_, i: number) => (
-                        <div key={i} className="Dashboard__assignment">
-                            <div><a href="#" className="Dashboard__assignmentLink">Assignment 3</a></div>
-                            <div>5 Sep, 10:00AM</div>
-                            <div>15 Sep, 11:59pm</div>
-                        </div>
+                        <TableRow key={i} className="Assignment__submission">
+                            <TableCell className="Dashboard__assignmentLink">Assignment 1</TableCell>
+                            <TableCell>5 Sep, 9:00 AM</TableCell>
+                            <TableCell>15 Sep, 11:59 PM</TableCell>
+                        </TableRow>
                     ))}
-                </div>
-                <div className="Dashboard__assignmentsHeader">
-                    Inactive Assignments (3)</div>
-                <div className="Dashboard__assignments">
-                    <div className="Dashboard__assignment">
-                        <div>Assignment Name</div>
-                        <div>Released</div>
-                        <div>Due Date</div>
-                    </div>
-                    {Array.from({ length: 3 }).map((_, i: number) => (
-                        <div key={i} className="Dashboard__assignment">
-                            <div><a href="#" className="Dashboard__assignmentLink">Assignment 1</a></div>
-                            <div>5 Sep, 10:00AM</div>
-                            <div>15 Sep, 11:59pm</div>
-                        </div>
+                </Table>
+
+                <h2 style={{ marginBottom: 0 }}>Inactive Assignments</h2>
+                <Table cols={3}>
+                    <TableRow style={{ borderTop: "none" }}>
+                        <TableCell>Assignment Name</TableCell>
+                        <TableCell>Released</TableCell>
+                        <TableCell>Due Date</TableCell>
+                    </TableRow>
+                    {Array.from({ length: 2 }).map((_, i: number) => (
+                        <TableRow key={i} className="Assignment__submission">
+                            <TableCell>Assignment 1</TableCell>
+                            <TableCell>5 Sep, 9:00 AM</TableCell>
+                            <TableCell>15 Sep, 11:59 PM</TableCell>
+                        </TableRow>
                     ))}
-                </div>
+                </Table>
             </div>
         </div>
     )
