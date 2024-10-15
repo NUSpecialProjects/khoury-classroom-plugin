bf3d623a4a7fb9c96bdccc3849664283bc2290ac
Sebastian Tremblay <tremblay.se@northeastern.edu>
2024-10-15 14:08:01 -0400

    updating temp cors policy

--- backend/internal/middleware/cors.go
+++ backend/internal/middleware/cors.go
@@ -7,7 +7,7 @@ import (
 
 func Cors() fiber.Handler {
 	return cors.New(cors.Config{
-		AllowOrigins:     "http://localhost:3000",
+		AllowOrigins:     "http://khoury-classroom-frontend-prod-east-2.s3-website.us-east-2.amazonaws.com/",
 		AllowMethods:     "GET, POST, PUT, PATCH, DELETE, OPTIONS",
 		AllowHeaders:     "Origin, Content-Type, Authorization, Accept, Set-Cookie",
 		AllowCredentials: true,
