bb16a7f9f549cf486943bcbd5b2b747f92a5ff8b
Sebastian Tremblay <tremblay.se@northeastern.edu>
2024-10-15 14:23:17 -0400

    resolving cors issue

--- backend/internal/middleware/cors.go
+++ backend/internal/middleware/cors.go
@@ -7,7 +7,7 @@ import (
 
 func Cors() fiber.Handler {
 	return cors.New(cors.Config{
-		AllowOrigins:     "http://khoury-classroom-frontend-prod-east-2.s3-website.us-east-2.amazonaws.com/",
+		AllowOrigins:     "http://khoury-classroom-frontend-prod-east-2.s3-website.us-east-2.amazonaws.com",
 		AllowMethods:     "GET, POST, PUT, PATCH, DELETE, OPTIONS",
 		AllowHeaders:     "Origin, Content-Type, Authorization, Accept, Set-Cookie",
 		AllowCredentials: true,
