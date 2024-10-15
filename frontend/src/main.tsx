9bae13d6dbadcc50124fbc0d08a1e1338ecf9f0e
Sebastian Tremblay <tremblay.se@northeastern.edu>
2024-10-15 13:56:39 -0400

    adding prefix slash to routes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -34,14 +34,14 @@ export default function App(): React.JSX.Element {
     <AuthContext.Provider value={{ isLoggedIn, login }}>
       <Router>
         <Routes>
-          <Route path="" element={<Pages.Login />} />
-          <Route path="oauth/callback" element={<Pages.Callback />} />
+          <Route path="/" element={<Pages.Login />} />
+          <Route path="/oauth/callback" element={<Pages.Callback />} />
           <Route path="/app/" element={<PrivateRoute element={<Layout />} />}>
-            <Route path="assignments" element={<Pages.Assignments />} />
-            <Route path="assignments/:id" element={<Pages.Assignment />} />
-            <Route path="grading" element={<Pages.Grading />} />
-            <Route path="settings" element={<Pages.Settings />} />
-            <Route path="dashboard" element={<Pages.Dashboard />} />
+            <Route path="/assignments" element={<Pages.Assignments />} />
+            <Route path="/assignments/:id" element={<Pages.Assignment />} />
+            <Route path="/grading" element={<Pages.Grading />} />
+            <Route path="/settings" element={<Pages.Settings />} />
+            <Route path="/dashboard" element={<Pages.Dashboard />} />
           </Route>
         </Routes>
       </Router>
