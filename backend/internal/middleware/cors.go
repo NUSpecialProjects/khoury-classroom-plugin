668ee3ac61426b10808303dc3183fb4575ba0337
Sebastian Tremblay <tremblay.se@northeastern.edu>
2024-10-16 09:32:28 -0400

    removing cooking header

--- backend/internal/middleware/cors.go
+++ backend/internal/middleware/cors.go
@@ -9,7 +9,7 @@ func Cors() fiber.Handler {
 	return cors.New(cors.Config{
 		AllowOrigins:     "http://khoury-classroom-frontend-prod-east-2.s3-website.us-east-2.amazonaws.com",
 		AllowMethods:     "GET, POST, PUT, PATCH, DELETE, OPTIONS",
-		AllowHeaders:     "Origin, Content-Type, Authorization, Accept, Set-Cookie",
+		AllowHeaders:     "Origin, Content-Type, Authorization, Accept",
 		AllowCredentials: true,
 	})
 }
