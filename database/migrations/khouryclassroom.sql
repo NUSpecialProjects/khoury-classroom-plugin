4ff831da09b26500a349d7f3ec7f420d5972ceb7
Alex Angione <111721125+alexangione419@users.noreply.github.com>
2024-10-16 09:43:39 -0400

    Feature/database updates (#48)
    * database redesign/refactor
    
    * re added test rows and clearned prints

--- database/migrations/khouryclassroom.sql
+++ database/migrations/khouryclassroom.sql
@@ -1,17 +1,8 @@
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE IF NOT EXISTS semesters (
   id SERIAL PRIMARY KEY,
-  role VARCHAR(50) NOT NULL,
-  name VARCHAR(255) NOT NULL,
-  gh_username VARCHAR(255) NOT NULL,
-  joined_on TIMESTAMP
-);
-
-CREATE TABLE IF NOT EXISTS professor_ta (
-  prof_id INTEGER NOT NULL,
-  ta_id INTEGER,
-  PRIMARY KEY (prof_id, ta_id),
-  FOREIGN KEY (prof_id) REFERENCES users(id),
-  FOREIGN KEY (ta_id) REFERENCES users(id)
+  classroom_id INTEGER UNIQUE NOT NULL,
+  active BOOLEAN NOT NULL,
+  org_id INTEGER UNIQUE NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS rubrics (
@@ -19,45 +10,42 @@ CREATE TABLE IF NOT EXISTS rubrics (
   content VARCHAR(255) NOT NULL 
 );
 
-CREATE TABLE IF NOT EXISTS assignment_templates (
+CREATE TABLE IF NOT EXISTS assignments (
   id SERIAL PRIMARY KEY,
   rubric_id INTEGER,
+  classroom_id INTEGER NOT NULL,
   FOREIGN KEY (rubric_id) REFERENCES rubrics(id)
 );
 
-CREATE TABLE IF NOT EXISTS assignments (
+CREATE TABLE IF NOT EXISTS student_assignments (
   id SERIAL PRIMARY KEY, 
   name VARCHAR(255) NOT NULL,
   description VARCHAR(255),
   student_gh_username VARCHAR(255) NOT NULL,
-  ta_id INTEGER NOT NULL,
-  github_classroom_id INTEGER NOT NULL,
-  template_id INTEGER NOT NULL,
+  ta_gh_username VARCHAR(255) NOT NULL,
+  assignment_id INTEGER NOT NULL,
   completed BOOLEAN NOT NULL,
   started BOOLEAN NOT NULL,
-  FOREIGN KEY (ta_id) REFERENCES users(id),
-  FOREIGN KEY (template_id) REFERENCES assignment_templates(id)
+  FOREIGN KEY (assignment_id) REFERENCES assignments(id)
 );
 
 CREATE TABLE IF NOT EXISTS due_dates (
   id SERIAL PRIMARY KEY,
   due TIMESTAMP DEFAULT NOW() NOT NULL,
-  assignment_id INTEGER NOT NULL,
-  FOREIGN KEY (assignment_id) REFERENCES assignments(id)
+  student_assignment_id INTEGER NOT NULL,
+  FOREIGN KEY (student_assignment_id) REFERENCES student_assignments(id)
 );
 
 CREATE TABLE IF NOT EXISTS regrades (
   id SERIAL PRIMARY KEY,
-  student_id INTEGER NOT NULL,
-  ta_id INTEGER NOT NULL,
+  student_gh_username VARCHAR(255) NOT NULL,
+  ta_gh_username VARCHAR(255) NOT NULL,
   due_date_id INTEGER NOT NULL,
-  FOREIGN KEY (student_id) REFERENCES users(id),
-  FOREIGN KEY (ta_id) REFERENCES users(id),
   FOREIGN KEY (due_date_id) REFERENCES due_dates(id)
 );
 
 CREATE TABLE IF NOT EXISTS sessions (
-  github_user_id INTEGER PRIMARY KEY,
+  github_user_id INTEGER UNIQUE PRIMARY KEY,
   access_token VARCHAR(255) NOT NULL,
   token_type VARCHAR(255),
   refresh_token VARCHAR(255),
