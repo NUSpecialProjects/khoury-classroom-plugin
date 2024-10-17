cf64d667ba58f26f638d5a0095cd256120c31aac
Nicholas Tietje <tietje.n@northeastern.edu>
2024-10-17 09:31:46 -0400

    add endpoints

--- backend/internal/handlers/github/routes.go
+++ backend/internal/handlers/github/routes.go
@@ -17,7 +17,23 @@ func Routes(app *fiber.App, params types.Params) {
 	protected.Get("/user", service.GetCurrentUser())
 	protected.Get("/classrooms", service.ListClassrooms())
 	protected.Post("/startup", service.AppInitialization())
-	protected.Get("/available-orgs", service.GetInstalledOrgs())
-	protected.Get("/org-classrooms", service.ListOrgClassrooms())
+
+	// list the orgs of the user with an our app installed
+	protected.Get("/user/orgs", service.GetInstalledOrgs())
+
+	// // list the orgs of the user
+	// protected.Get("/user/all-orgs", service.GetUserOrgs())
+
+	// // list the classrooms that are already associated with a "semester" in our app
+	// protected.Get("/user/semesters", service.GetSemesters())
+
+	protected.Post("/semesters", service.CreateSemester())
+
+	// list the classrooms that the user has in the given org
+	protected.Get("/user/orgs/:org/classrooms", service.ListOrgClassrooms())
+
+	// get the user's roles (in permission level order)
+	protected.Get("/user/roles", service.GetUserRoles())
+
 	app.Post("/webhook", middleware.ProtectedWebhook(params.GitHubApp.GetWebhookSecret()), service.WebhookHandler)
 }
