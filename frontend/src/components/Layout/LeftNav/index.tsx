9c5e0cdd81cbd5326ab0633022a065d62cd63c32
Nandini Ghosh <93226556+nandini-ghosh@users.noreply.github.com>
2024-10-17 14:06:27 -0400

    Frontend details for the Dashboard page (#41)
    * Dashboard classroom details
    
    * component user card done + more dashboard details
    
    * dashboard details completed
    
    * table component
    
    * underlining for clickable table item
    
    * a links
    
    * link element to replace a href
    
    * classroom name added to left nav menu
    
    * icons modified
    
    ---------
    
    Co-authored-by: Sebastian Tremblay <91509344+sebytremblay@users.noreply.github.com>

--- frontend/src/components/Layout/LeftNav/index.tsx
+++ frontend/src/components/Layout/LeftNav/index.tsx
@@ -1,4 +1,5 @@
-import { FaTachometerAlt, FaStream, FaUsers, FaCog } from "react-icons/fa";
+import { FaTachometerAlt, FaCog } from "react-icons/fa";
+import { MdFactCheck, MdEditDocument } from "react-icons/md";
 import { Link } from "react-router-dom";
 
 import "./styles.css";
@@ -6,14 +7,17 @@ import "./styles.css";
 const LeftNav: React.FC = () => {
   const navItems = [
     { name: "Dashboard", dest: "/app/dashboard", Icon: FaTachometerAlt },
-    { name: "Grading", dest: "/app/grading", Icon: FaStream },
-    { name: "Assignments", dest: "/app/assignments", Icon: FaUsers },
+    { name: "Grading", dest: "/app/grading", Icon: MdFactCheck },
+    { name: "Assignments", dest: "/app/assignments", Icon: MdEditDocument },
     { name: "Settings", dest: "/app/settings", Icon: FaCog },
     { name: "Kenny", dest: "/app/assignments/1", Icon: FaCog },
   ];
 
+  const className = "CS 3200 Database Design"
+
   return (
     <div className="LeftNav">
+      <div className="LeftNav__classNameHeader">{className}</div>
       {navItems.map((item, index) => (
         <Link key={index} to={item.dest} className="LeftNav__link">
           <item.Icon /> {item.name}
