7281c6c4be12b3ff514a9c6dd41a8f19d1e02187
kennybc <54950614+kennybc@users.noreply.github.com>
2024-10-17 13:24:21 -0400

    refactor impacted references

--- backend/internal/storage/postgres/assignments.go
+++ backend/internal/storage/postgres/assignments.go
@@ -10,43 +10,43 @@ import (
 
 func (db *DB) GetAllAssignments(ctx context.Context) ([]models.Assignment, error) {
 
-  rows, err := db.connPool.Query(ctx, "SELECT (rubric_id, classroom_id) FROM assignments")
+	rows, err := db.connPool.Query(ctx, "SELECT (local_id, rubric_id, semester_id, name) FROM assignments")
 
-  if err != nil {
-    fmt.Println("Error in query")
-    return nil, err 
-  }
+	if err != nil {
+		fmt.Println("Error in query")
+		return nil, err
+	}
 
-  defer rows.Close()
+	defer rows.Close()
 	return pgx.CollectRows(rows, pgx.RowToStructByName[models.Assignment])
 }
 
-func (db *DB) CreateAssignment(ctx context.Context, assignmentData models.Assignment) (error) {
-  _, err := db.connPool.Exec(ctx, "INSERT INTO assignments (rubric_id, classroom_id) VALUES ($1, $2)",
-    assignmentData.Rubric_ID,
-    assignmentData.Classroom_ID)
-  if err != nil {
-    return err
-  }
+func (db *DB) CreateAssignment(ctx context.Context, assignmentData models.Assignment) error {
+	_, err := db.connPool.Exec(ctx, "INSERT INTO assignments (rubric_id, semester_id, name) VALUES ($1, $2, $3)",
+		assignmentData.RubricID,
+		assignmentData.SemesterID,
+		assignmentData.Name)
+	if err != nil {
+		return err
+	}
 
-  return nil
+	return nil
 }
 
-func (db *DB) CreateStudentAssignment(ctx context.Context, assignmentData models.StudentAssignment) (error) {
-  _, err := db.connPool.Exec(ctx,
-    "INSERT INTO student_assignments (name, description, student_gh_username, ta_gh_username, completed, started, assignment_id) VALUES ($1, $2, $3, $4, $5, $6, $7)", 
-    assignmentData.Name,
-    assignmentData.Description,
-    assignmentData.Student_GH_Username,
-    assignmentData.TA_GH_Username,
-    assignmentData.Completed,
-    assignmentData.Started,
-    assignmentData.Assignment_ID)
-  if err != nil {
-    fmt.Println("Error in con pool exec")
-    fmt.Println(err.Error())
-    return err
-  }
+func (db *DB) CreateStudentAssignment(ctx context.Context, assignmentData models.StudentAssignment) error {
+	_, err := db.connPool.Exec(ctx,
+		"INSERT INTO student_assignments (assignment_id, repo_name, student_gh_username, ta_gh_username, completed, started) VALUES ($1, $2, $3, $4, $5, $6, $7)",
+		assignmentData.AssignmentID,
+		assignmentData.RepoName,
+		assignmentData.StudentGHUsername,
+		assignmentData.TAGHUsername,
+		assignmentData.Completed,
+		assignmentData.Started)
+	if err != nil {
+		fmt.Println("Error in con pool exec")
+		fmt.Println(err.Error())
+		return err
+	}
 
-  return nil
+	return nil
 }
