2443d50ca1d8111efb022aad8c8c10860a378004
Nicholas Tietje <tietje.n@northeastern.edu>
2024-10-18 15:40:53 -0400

    add button to get to dashboard

--- frontend/src/pages/Semesters/Selection/index.tsx
+++ frontend/src/pages/Semesters/Selection/index.tsx
@@ -1,10 +1,14 @@
 // import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "./styles.css";
 
 
 const SemesterSelection: React.FC = () => {
+    const navigate = useNavigate();
+
     return <div>
         <h1>Select a Semester</h1>
+        <button onClick={() => { navigate("/app/dashboard") }}> Go to Dashboard Page</button>
     </div>;
 }
 
