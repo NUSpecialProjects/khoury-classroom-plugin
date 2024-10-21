a26defa90a3914b8ee2513c3baf852b51bb125bc
nandini-ghosh <nandini.ghosh2094@gmail.com>
2024-10-21 14:55:39 -0400

    menu and layout panels css

--- frontend/src/components/Layout/TopNav/index.tsx
+++ frontend/src/components/Layout/TopNav/index.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import UserProfilePic from "../../UserProfilePic";
 
 import "./styles.css";
@@ -7,10 +6,6 @@ import "./styles.css";
 const TopNav: React.FC = () => {
   return (
     <div className="TopNav">
-      <div className="TopNav__left">
-        <Link to="/">Khoury Classroom</Link>
-      </div>
-
       <div className="TopNav__right">
         <UserProfilePic />
       </div>
