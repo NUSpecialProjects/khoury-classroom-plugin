0ff66715f512b369dffa92ebbc60ca86c1690aaa
Nandini Ghosh <93226556+nandini-ghosh@users.noreply.github.com>
2024-10-23 10:08:07 -0400

    Hi-fi Styling (#66)
    * Branch for styling, css and cleanup + css variables for global
    
    * semicolon womp womp
    
    * global CSS
    
    * menu and layout panels css
    
    * dashboard styling
    
    * assignments, tables and menu fixes
    
    * button
    
    * cookies?
    
    * trying to fix bug
    
    * fix properties
    
    * div fix
    
    ---------
    
    Co-authored-by: Nick <tietje.n@northeastern.edu>

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -11,7 +11,6 @@ import ErrorMessage from "@/components/Error";
 const Dashboard: React.FC = () => {
   const { selectedSemester, setSelectedSemester } = useSelectedSemester();
   const [error, setError] = useState<string | null>(null);
-
   const handleActivate = async (newSemester: ISemester) => {
     setSelectedSemester(newSemester);
   };
@@ -67,53 +66,57 @@ const Dashboard: React.FC = () => {
             <UserGroupCard label="Students" number={38} />
           </div>
           <div className="Dashboard__assignmentsWrapper">
-            <h2 style={{ marginBottom: 0 }}>Active Assignments</h2>
-            <Table cols={3}>
-              <TableRow style={{ borderTop: "none" }}>
-                <TableCell>Assignment Name</TableCell>
-                <TableCell>Released</TableCell>
-                <TableCell>Due Date</TableCell>
-              </TableRow>
-              {Array.from({ length: 1 }).map((_, i: number) => (
-                <TableRow key={i} className="Assignment__submission">
-                  <TableCell>
-                    {" "}
-                    <Link
-                      to="/app/assignments/assignmentdetails"
-                      className="Dashboard__assignmentLink"
-                    >
-                      Assignment 1
-                    </Link>
-                  </TableCell>
-                  <TableCell>5 Sep, 9:00 AM</TableCell>
-                  <TableCell>15 Sep, 11:59 PM</TableCell>
+            <div className="Dashboard__assignmentsWrapper">
+              <h1>Active Assignments</h1>
+              <Table cols={3}>
+                <TableRow style={{ borderTop: "none" }}>
+                  <TableCell>Assignment Name</TableCell>
+                  <TableCell>Released</TableCell>
+                  <TableCell>Due Date</TableCell>
                 </TableRow>
-              ))}
-            </Table>
+                {Array.from({ length: 1 }).map((_, i: number) => (
+                  <TableRow key={i} className="Assignment__submission">
+                    <TableCell>
+                      {" "}
+                      <Link
+                        to="/app/assignments/assignmentdetails"
+                        className="Dashboard__assignmentLink"
+                      >
+                        Assignment 1
+                      </Link>
+                    </TableCell>
+                    <TableCell>5 Sep, 9:00 AM</TableCell>
+                    <TableCell>15 Sep, 11:59 PM</TableCell>
+                  </TableRow>
+                ))}
+              </Table>
+            </div>
 
-            <h2 style={{ marginBottom: 0 }}>Inactive Assignments</h2>
-            <Table cols={3}>
-              <TableRow style={{ borderTop: "none" }}>
-                <TableCell>Assignment Name</TableCell>
-                <TableCell>Released</TableCell>
-                <TableCell>Due Date</TableCell>
-              </TableRow>
-              {Array.from({ length: 2 }).map((_, i: number) => (
-                <TableRow key={i} className="Assignment__submission">
-                  <TableCell>
-                    {" "}
-                    <Link
-                      to="/app/assignments/assignmentdetails"
-                      className="Dashboard__assignmentLink"
-                    >
-                      Assignment 1
-                    </Link>
-                  </TableCell>
-                  <TableCell>5 Sep, 9:00 AM</TableCell>
-                  <TableCell>15 Sep, 11:59 PM</TableCell>
+            <div className="Dashboard__assignmentsWrapper">
+              <h1>Inactive Assignments</h1>
+              <Table cols={3}>
+                <TableRow style={{ borderTop: "none" }}>
+                  <TableCell>Assignment Name</TableCell>
+                  <TableCell>Released</TableCell>
+                  <TableCell>Due Date</TableCell>
                 </TableRow>
-              ))}
-            </Table>
+                {Array.from({ length: 2 }).map((_, i: number) => (
+                  <TableRow key={i} className="Assignment__submission">
+                    <TableCell>
+                      {" "}
+                      <Link
+                        to="/app/assignments/assignmentdetails"
+                        className="Dashboard__assignmentLink"
+                      >
+                        Assignment 1
+                      </Link>
+                    </TableCell>
+                    <TableCell>5 Sep, 9:00 AM</TableCell>
+                    <TableCell>15 Sep, 11:59 PM</TableCell>
+                  </TableRow>
+                ))}
+              </Table>
+            </div>
           </div>
           <div>
             <p>Temporary Classroom Settings</p>
