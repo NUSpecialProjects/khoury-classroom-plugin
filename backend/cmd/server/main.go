9c56dc17a0fdc0872ca228854a80f0533bd61828
Sebastian Tremblay <91509344+sebytremblay@users.noreply.github.com>
2024-10-25 10:21:01 -0400

    fixing dev script (#87)

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -26,7 +26,7 @@ func main() {
 
 	// Load environment variables if running locally
 	if isLocal() {
-		if err := godotenv.Load("../../../.env"); err != nil {
+		if err := godotenv.Load(".env"); err != nil {
 			log.Fatalf("Unable to load environment variables necessary for application: %v", err)
 		}
 	}
