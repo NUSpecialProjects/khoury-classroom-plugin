5e1ebe59abf93e5f15c8981347cf5da440ff81c3
Nick <tietje.n@northeastern.edu>
2024-10-29 11:16:25 -0400

    add comment

--- backend/internal/utils/tokens.go
+++ backend/internal/utils/tokens.go
@@ -1,5 +1,6 @@
 package utils
 
+// TODO: this will be used to generate tokens for urls
 // func generateToken(length uint32) (string, error) {
 // 	bytes := make([]byte, length)
 // 	if _, err := rand.Read(bytes); err != nil {
