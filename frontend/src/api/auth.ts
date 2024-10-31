7a6533eaba7ac4ecb678102937a638160c627e88
Nick <tietje.n@northeastern.edu>
2024-10-31 15:09:41 -0400

    fix endpoints and add mocked implementations

--- frontend/src/api/auth.ts
+++ frontend/src/api/auth.ts
@@ -2,7 +2,7 @@ const base_url: string = import.meta.env.VITE_PUBLIC_API_DOMAIN as string;
 
 export const getCurrentUser = async (): Promise<boolean> => {
   try {
-    const result = await fetch(`${base_url}/github/user`, {
+    const result = await fetch(`${base_url}/user`, {
       method: "GET",
       credentials: "include",
       headers: {
