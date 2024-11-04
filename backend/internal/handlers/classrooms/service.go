a4a98abba6c89ca4e71a6ccdea179a4f174df198
Alex Angione <111721125+alexangione419@users.noreply.github.com>
2024-11-04 14:39:41 -0500

    protected middleware

--- backend/internal/handlers/classrooms/service.go
+++ backend/internal/handlers/classrooms/service.go
@@ -1,11 +1,13 @@
 package classrooms
 
 import (
+	"github.com/CamPlume1/khoury-classroom/internal/config"
 	"github.com/CamPlume1/khoury-classroom/internal/storage"
 )
 
 type ClassroomService struct {
-	store storage.Storage
+	store   storage.Storage
+	userCfg *config.GitHubUserClient
 }
 
 func newClassroomService(store storage.Storage) *ClassroomService {
