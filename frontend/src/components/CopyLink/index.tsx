b8e2826c6fdc4498343f12a5476349fae9da51f9
nandini-ghosh <nandini.ghosh2094@gmail.com>
2024-11-08 11:16:41 -0500

    CopyLink component created

---
+++ frontend/src/components/CopyLink/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import './styles.css';
+import { FaRegCopy } from "react-icons/fa";
+
+interface ICopyLinkProps {
+    link: string;
+    name: string;
+}
+
+const CopyLink: React.FC<ICopyLinkProps> = ({
+    link,
+    name
+}) => {
+    const copyLink = async() => {
+        try {
+            await navigator.clipboard.writeText(link);
+            alert('Link copied to clipboard!');
+        } catch (error) {
+            console.error('Failed to copy the link: ', error);
+            alert('Error copying link, please try again.');
+        }
+    }
+
+    return (
+        <div className="CopyLink">
+            <input id={name} type="text" value={link} className="CopyLink__link" readOnly></input>
+            <button onClick={copyLink} className="CopyLink__copyButton"><FaRegCopy /></button>
+        </div>
+    );
+};
+
+export default CopyLink;
