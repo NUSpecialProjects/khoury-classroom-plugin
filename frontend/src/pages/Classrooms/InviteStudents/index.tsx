fdf22d6b42fa1876d00bccf0f070cd6aa476eea5
nandini-ghosh <nandini.ghosh2094@gmail.com>
2024-11-08 13:56:17 -0500

    CopyLink modified and added to invitation screens

--- frontend/src/pages/Classrooms/InviteStudents/index.tsx
+++ frontend/src/pages/Classrooms/InviteStudents/index.tsx
@@ -1,11 +1,38 @@
 import Panel from "@/components/Panel";
 import Button from "@/components/Button";
-import { useNavigate, Link } from "react-router-dom";
+import CopyLink from "@/components/CopyLink";
+import { useState, useContext, useEffect } from "react";
+import { SelectedClassroomContext } from "@/contexts/selectedClassroom";
+import { useNavigate } from "react-router-dom";
+import { postClassroomToken } from "@/api/classrooms";
+
 
 import "../styles.css";
 
 const InviteStudents: React.FC = () => {
 const navigate = useNavigate();
+const { selectedClassroom } = useContext(SelectedClassroomContext);
+const [link, setLink] = useState<string>("");
+
+useEffect(() => {
+    const handleCreateToken = async () => {
+        if (!selectedClassroom) {
+            return;
+        }
+        await postClassroomToken(selectedClassroom.id, "TA")
+            .then((data: ITokenResponse) => {
+                const url = "http://localhost:3000/app/token/apply?token=" + data.token;
+                setLink(url);
+            })
+            .catch((error) => {
+                console.log("Error creating token: " + error);
+            });
+    };
+
+    if (selectedClassroom) {
+        handleCreateToken();
+    }
+}, [selectedClassroom])
 
     return (
         <Panel title="Add Students" logo={true}>
@@ -13,8 +40,9 @@ const navigate = useNavigate();
                 <div className="Invite__ContentWrapper">
                     <div className="Invite__TextWrapper">
                         <h2>Use the link below to invite students</h2>
-                        <div>To add students to your classroom, invite them using this link!</div>
+                        <div>{"To add students to your classroom, invite them using this link!"}</div>
                     </div>
+                    <CopyLink link={link} name="invite-students"></CopyLink>
                 </div>
                 <div className="ButtonWrapper">
                     <Button variant="primary" onClick={() => navigate("/app/classroom/success")}>Continue</Button>
--- frontend/src/pages/Classrooms/InviteTAs/index.tsx
+++ frontend/src/pages/Classrooms/InviteTAs/index.tsx
@@ -1,11 +1,37 @@
 import Panel from "@/components/Panel";
 import Button from "@/components/Button";
-import { useNavigate, Link } from "react-router-dom";
+import CopyLink from "@/components/CopyLink";
+import { useNavigate } from "react-router-dom";
+import { useState, useContext, useEffect } from "react";
+import { SelectedClassroomContext } from "@/contexts/selectedClassroom";
+import { postClassroomToken } from "@/api/classrooms";
 
 import "../styles.css";
 
 const InviteTAs: React.FC = () => {
-const navigate = useNavigate();
+    const navigate = useNavigate();
+    const { selectedClassroom } = useContext(SelectedClassroomContext);
+    const [link, setLink] = useState<string>("");
+
+    useEffect(() => {
+        const handleCreateToken = async () => {
+            if (!selectedClassroom) {
+                return;
+            }
+            await postClassroomToken(selectedClassroom.id, "TA")
+                .then((data: ITokenResponse) => {
+                    const url = "http://localhost:3000/app/token/apply?token=" + data.token;
+                    setLink(url);
+                })
+                .catch((error) => {
+                    console.log("Error creating token: " + error);
+                });
+        };
+
+        if (selectedClassroom) {
+            handleCreateToken();
+        }
+    }, [selectedClassroom])
 
     return (
         <Panel title="Add Teaching Assistants" logo={true}>
@@ -13,8 +39,9 @@ const navigate = useNavigate();
                 <div className="Invite__ContentWrapper">
                     <div className="Invite__TextWrapper">
                         <h2>Use the link below to invite TAs to your Classroom</h2>
-                        <div>To add TA’s to your classroom, invite them using this link!</div>
+                        <div>{"To add TA’s to your classroom, invite them using this link!"}</div>
                     </div>
+                    <CopyLink link={link} name="invite-tas"></CopyLink>
                 </div>
                 <div className="ButtonWrapper">
                     <Button variant="primary" onClick={() => navigate("/app/classroom/invite-students")}>Continue</Button>
