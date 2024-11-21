012ec02fe20b5fbaa8b9bb5a103c5ee065d4df72
Kenneth Chen <54950614+kennybc@users.noreply.github.com>
2024-11-21 10:20:33 -0500

    KHO-201 (#145)
    * remove assignment outline id
    
    * update mock data
    
    * fix hotfix

--- backend/internal/models/feedback_comment.go
+++ backend/internal/models/feedback_comment.go
@@ -3,13 +3,12 @@ package models
 import "time"
 
 type FeedbackComment struct {
-	AssignmentOutlineID *int      `json:"assignment_outline_id"`
-	StudentWorkID       int       `json:"student_work_id"`
-	RubricItemID        int       `json:"rubric_item_id"`
-	TAUsername          string    `json:"github_username" db:"github_username"`
-	PointValue          int       `json:"point_value"`
-	Explanation         string    `json:"explanation"`
-	FilePath            *string   `json:"file_path"`
-	FileLine            *int      `json:"file_line"`
-	CreatedAt           time.Time `json:"created_at"`
+	StudentWorkID int       `json:"student_work_id"`
+	RubricItemID  int       `json:"rubric_item_id"`
+	TAUsername    string    `json:"github_username" db:"github_username"`
+	PointValue    int       `json:"point_value"`
+	Explanation   string    `json:"explanation"`
+	FilePath      *string   `json:"file_path"`
+	FileLine      *int      `json:"file_line"`
+	CreatedAt     time.Time `json:"created_at"`
 }
