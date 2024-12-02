f416b3dc30d3cc9c7128d8ec9c90f6d86eeb1321
nandini-ghosh <nandini.ghosh2094@gmail.com>
2024-12-02 13:26:59 -0500

    padding added to bottom of pages

--- frontend/src/pages/Settings/index.tsx
+++ frontend/src/pages/Settings/index.tsx
@@ -2,12 +2,13 @@ import { AuthContext } from "@/contexts/auth";
 import { useContext } from "react";
 import PageHeader from "@/components/PageHeader";
 import Button from "@/components/Button";
+import './styles.css';
 
 const Settings: React.FC = () => {
   const { logout } = useContext(AuthContext);
 
   return (
-    <div>
+    <div className="Settings">
       <PageHeader pageTitle="Settings"></PageHeader>
       <Button variant="primary" onClick={logout}>
         Logout
