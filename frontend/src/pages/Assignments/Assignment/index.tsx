69f7feabc589c14b5224a3b5099affd0ffe5738f
kennybc <54950614+kennybc@users.noreply.github.com>
2024-12-02 17:02:48 -0500

    initial charts

--- frontend/src/pages/Assignments/Assignment/index.tsx
+++ frontend/src/pages/Assignments/Assignment/index.tsx
@@ -1,15 +1,24 @@
-import Button from "@/components/Button";
-
-import "./styles.css";
 import { useLocation, useParams } from "react-router-dom";
 import { useContext, useEffect, useState } from "react";
+import { Chart as ChartJS, registerables } from "chart.js";
+import { Bar, Pie } from "react-chartjs-2";
+
 import { SelectedClassroomContext } from "@/contexts/selectedClassroom";
-import { Table, TableCell, TableRow } from "@/components/Table";
-import SubPageHeader from "@/components/PageHeader/SubPageHeader";
-import { getAssignmentIndirectNav, postAssignmentToken } from "@/api/assignments";
+import {
+  getAssignmentIndirectNav,
+  postAssignmentToken,
+} from "@/api/assignments";
 import { getStudentWorks } from "@/api/student_works";
 import { formatDate } from "@/utils/date";
+
+import { Table, TableCell, TableRow } from "@/components/Table";
+import SubPageHeader from "@/components/PageHeader/SubPageHeader";
+import Button from "@/components/Button";
 import CopyLink from "@/components/CopyLink";
+
+import "./styles.css";
+
+ChartJS.register(...registerables);
 
 const Assignment: React.FC = () => {
   const location = useLocation();
@@ -19,7 +28,20 @@ const Assignment: React.FC = () => {
   const { id } = useParams();
   const [inviteLink, setInviteLink] = useState<string>("");
   const [linkError, setLinkError] = useState<string | null>(null);
-  const base_url: string = import.meta.env.VITE_PUBLIC_FRONTEND_DOMAIN as string;
+  const base_url: string = import.meta.env
+    .VITE_PUBLIC_FRONTEND_DOMAIN as string;
+
+  const data = {
+    labels: ["January", "February", "March", "April", "May", "June"],
+    datasets: [
+      {
+        label: "My First dataset", // Setting up the label for the dataset
+        backgroundColor: "rgb(255, 99, 132)", // Setting up the background color for the dataset
+        borderColor: "rgb(255, 99, 132)", // Setting up the border color for the dataset
+        data: [0, 10, 5, 2, 20, 30, 45], // Setting up the data for the dataset
+      },
+    ],
+  };
 
   useEffect(() => {
     // check if assignment has been passed through
@@ -73,10 +95,13 @@ const Assignment: React.FC = () => {
   useEffect(() => {
     const generateInviteLink = async () => {
       if (!assignment) return;
-      
+
       try {
         if (!selectedClassroom) return;
-        const tokenData = await postAssignmentToken(selectedClassroom.id, assignment.id);
+        const tokenData = await postAssignmentToken(
+          selectedClassroom.id,
+          assignment.id
+        );
         const url = `${base_url}/app/token/assignment/accept?token=${tokenData.token}`;
         setInviteLink(url);
       } catch (_) {
@@ -84,34 +109,33 @@ const Assignment: React.FC = () => {
       }
     };
 
-
     generateInviteLink();
   }, [assignment]);
 
   return (
-    <div className="Assignment">
-      {assignment && (
-        <>
-          <SubPageHeader
-            pageTitle={assignment.name}
-            chevronLink={"/app/dashboard"}
-          >
-            <div className="Assignment__dates">
-              <div className="Assignment__date">
-                <div className="Assignment__date--title"> {"Released on:"}</div>
-                {assignment.created_at
-                  ? formatDate(new Date(assignment.created_at))
-                  : "N/A"}
-              </div>
-              <div className="Assignment__date">
-                <div className="Assignment__date--title"> {"Due Date:"}</div>
-                {assignment.main_due_date
-                  ? formatDate(new Date(assignment.main_due_date))
-                  : "N/A"}
-              </div>
+    assignment && (
+      <>
+        <SubPageHeader
+          pageTitle={assignment.name}
+          chevronLink={"/app/dashboard"}
+        >
+          <div className="Assignment__dates">
+            <div className="Assignment__date">
+              <div className="Assignment__date--title"> {"Released on:"}</div>
+              {assignment.created_at
+                ? formatDate(new Date(assignment.created_at))
+                : "N/A"}
             </div>
-          </SubPageHeader>
+            <div className="Assignment__date">
+              <div className="Assignment__date--title"> {"Due Date:"}</div>
+              {assignment.main_due_date
+                ? formatDate(new Date(assignment.main_due_date))
+                : "N/A"}
+            </div>
+          </div>
+        </SubPageHeader>
 
+        <div className="Assignment">
           <div className="Assignment__externalButtons">
             <Button href="" variant="secondary" newTab>
               View in Github Classroom
@@ -124,16 +148,25 @@ const Assignment: React.FC = () => {
             </Button>
           </div>
 
-          <h2>Assignment Link</h2>
-          <CopyLink link={inviteLink} name="invite-assignment" />
-          {linkError && <p className="error">{linkError}</p>}
-
-          <div className="Assignment__subSectionWrapper">
-            <h2>Metrics</h2>
-            <p>Metrics go here</p>
+          <div className="Assignment__link">
+            <h2>Assignment Link</h2>
+            <CopyLink link={inviteLink} name="invite-assignment" />
+            {linkError && <p className="error">{linkError}</p>}
           </div>
 
-          <div className="Assignment__subSectionWrapper">
+          <div className="Assignment__metrics">
+            <h2>Metrics</h2>
+            <div className="Assignment__metricsCharts">
+              <div className="Assignment__metricsChart">
+                <Bar data={data} />
+              </div>
+              <div className="Assignment__metricsChart">
+                <Pie data={data} />
+              </div>
+            </div>
+          </div>
+
+          <div>
             <h2 style={{ marginBottom: 0 }}>Student Assignments</h2>
             <Table cols={3}>
               <TableRow style={{ borderTop: "none" }}>
@@ -152,9 +185,9 @@ const Assignment: React.FC = () => {
                 ))}
             </Table>
           </div>
-        </>
-      )}
-    </div>
+        </div>
+      </>
+    )
   );
 };
 
