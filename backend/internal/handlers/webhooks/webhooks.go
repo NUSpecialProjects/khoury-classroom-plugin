9500a8432c3fd9ab006d648a0008259d52ae4019
Sebastian Tremblay <tremblay.se@northeastern.edu>
2024-12-04 14:07:43 -0800

    catching students with no names

--- backend/internal/handlers/webhooks/webhooks.go
+++ backend/internal/handlers/webhooks/webhooks.go
@@ -70,7 +70,7 @@ func (s *WebHookService) PushEvent(c *fiber.Ctx) error {
 	}
 
 	// If app bot triggered the initial commit, initialize the base repository
-	if isInitialCommit && hasNamedPusher(pushEvent) && isBotPushEvent(pushEvent) {
+	if isInitialCommit && isBotPushEvent(pushEvent) {
 		err = s.baseRepoInitialization(c, pushEvent)
 		if err != nil {
 			return err
@@ -78,7 +78,7 @@ func (s *WebHookService) PushEvent(c *fiber.Ctx) error {
 	}
 
 	// If students pushed commits, update the work state accordingly
-	if hasNamedPusher(pushEvent) && !isBotPushEvent(pushEvent) && pushEvent.Commits != nil && len(pushEvent.Commits) > 0 {
+	if !isBotPushEvent(pushEvent) && pushEvent.Commits != nil && len(pushEvent.Commits) > 0 {
 		err = s.updateWorkStateOnStudentCommit(c, pushEvent)
 		if err != nil {
 			return err
@@ -88,12 +88,10 @@ func (s *WebHookService) PushEvent(c *fiber.Ctx) error {
 	return nil
 }
 
-func hasNamedPusher(pushEvent github.PushEvent) bool {
-	return pushEvent.Pusher != nil && pushEvent.Pusher.Name != nil
-}
-
 func isBotPushEvent(pushEvent github.PushEvent) bool {
-	return strings.Contains(*pushEvent.Pusher.Name, "[bot]")
+	return pushEvent.Pusher != nil &&
+		pushEvent.Pusher.Name != nil &&
+		strings.Contains(*pushEvent.Pusher.Name, "[bot]")
 }
 
 func (s *WebHookService) baseRepoInitialization(c *fiber.Ctx, pushEvent github.PushEvent) error {
