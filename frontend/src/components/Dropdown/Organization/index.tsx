24870ef773e4a21b48422ead50bc8a6c9ffacc53
Nick <tietje.n@northeastern.edu>
2024-12-04 14:46:28 -0500

    update old app name

--- frontend/src/components/Dropdown/Organization/index.tsx
+++ frontend/src/components/Dropdown/Organization/index.tsx
@@ -20,7 +20,7 @@ const OrganizationDropdown: React.FC<Props> = ({
     const options: IDropdownOption[] = [];
 
     if (orgsWithApp.length > 0) {
-      options.push({ value: "header-1", label: "Organizations with GitGrader Installed", disabled: true });
+      options.push({ value: "header-1", label: "Organizations with Marks Installed", disabled: true });
       orgsWithApp.forEach(org => {
         options.push({
           value: org.login,
@@ -30,7 +30,7 @@ const OrganizationDropdown: React.FC<Props> = ({
     }
 
     if (orgsWithoutApp.length > 0) {
-      options.push({ value: "header-2", label: "Organizations without GitGrader Installed", disabled: true });
+      options.push({ value: "header-2", label: "Organizations without Marks Installed", disabled: true });
       orgsWithoutApp.forEach(org => {
         options.push({
           value: org.login,
