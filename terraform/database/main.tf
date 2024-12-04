8e076613ab7c889e97af1f195d0fd743e1984a37
Sebastian Tremblay <tremblay.se@northeastern.edu>
2024-12-04 13:24:52 -0800

    db renaming

--- terraform/database/main.tf
+++ terraform/database/main.tf
@@ -5,6 +5,7 @@ resource "aws_db_instance" "main" {
   engine                 = "postgres"
   engine_version         = "16.3"
   instance_class         = "db.t3.micro"
+  identifier             = var.db_name
   db_name                = var.db_name
   username               = var.db_user
   password               = var.db_password
