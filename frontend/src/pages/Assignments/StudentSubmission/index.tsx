c26719d0c5df8c1f6911aeca8190581e1e3c8b71
nandini-ghosh <nandini.ghosh2094@gmail.com>
2024-12-05 16:43:24 -0500

    submission page

--- frontend/src/pages/Assignments/StudentSubmission/index.tsx
+++ frontend/src/pages/Assignments/StudentSubmission/index.tsx
@@ -1,10 +1,30 @@
 import "./styles.css";
+import { useLocation, useParams, Link } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import SubPageHeader from "@/components/PageHeader/SubPageHeader";
+import MetricPanel from "@/components/Metrics/MetricPanel";
+import SimpleMetric from "@/components/Metrics/SimpleMetric";
+import { getPaginatedStudentWork } from "@/api/student_works";
+
 
 const StudentSubmission: React.FC = () => {
-  return (
-    <div className="StudentWork">
-    </div>
-  );
+    const location = useLocation();
+    const [submission, setSubmission] = useState<IStudentWork[]>([]);
+    const { id } = useParams();
+    // const { selectedClassroom } = useContext(SelectedClassroomContext);
+
+    useEffect(() => {
+        // check if assignment has been passed through
+        if (location.state) {
+            setSubmission(location.state.submission);
+        }
+        console.log(submission, id);
+    }, []);
+
+    return (
+        <div className="StudentWork">
+        </div>
+    );
 };
 
 export default StudentSubmission;
