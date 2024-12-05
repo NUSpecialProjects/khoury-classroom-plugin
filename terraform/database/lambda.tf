c07c24e6984292e7a9d57573245c3ce80d35070f
Sebastian Tremblay <tremblay.se@northeastern.edu>
2024-12-05 11:14:06 -0500

    cleanup

--- terraform/database/lambda.tf
+++ terraform/database/lambda.tf
@@ -2,7 +2,7 @@
 
 resource "aws_lambda_function" "drop_db_function" {
   filename         = "./database/func/drop_db.zip"
-  function_name    = "drop_db"
+  function_name    = "gitmarks-reinitialize-rds-schema"
   role             = aws_iam_role.lambda_execution_role.arn
   handler          = "drop_db.lambda_handler"
   runtime          = "python3.9"
