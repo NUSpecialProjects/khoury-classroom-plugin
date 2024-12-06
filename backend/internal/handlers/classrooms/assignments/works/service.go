8dfe2f28b8e3276f76f7502f4a78de00fbf19ed5
Nick Tietje <124538220+ntietje1@users.noreply.github.com>
2024-12-06 11:05:02 -0500

    Feature/role checking (KHO-196, KHO-207) (#165)
    * add access denied message for students + better loading state
    
    * add role checking
    
    * add student landing page
    
    * make sebby a prof
    
    * temp disable require role on works
    
    * add org name to landing page
    
    * remove prints
    
    * add clarifying comment
    
    * fixes
    
    * fix hardcoded string

--- backend/internal/handlers/classrooms/assignments/works/service.go
+++ backend/internal/handlers/classrooms/assignments/works/service.go
@@ -1,49 +1,34 @@
 package works
 
 import (
-	"strconv"
-
 	"github.com/CamPlume1/khoury-classroom/internal/config"
-	"github.com/CamPlume1/khoury-classroom/internal/errs"
 	"github.com/CamPlume1/khoury-classroom/internal/github"
-	"github.com/CamPlume1/khoury-classroom/internal/models"
+	"github.com/CamPlume1/khoury-classroom/internal/middleware"
 	"github.com/CamPlume1/khoury-classroom/internal/storage"
-	"github.com/gofiber/fiber/v2"
 )
 
 type WorkService struct {
 	store     storage.Storage
 	userCfg   *config.GitHubUserClient
 	appClient github.GitHubAppClient
+	middleware.RoleChecker[WorkService]
 }
 
 func NewWorkService(store storage.Storage, userCfg *config.GitHubUserClient, appClient github.GitHubAppClient) *WorkService {
 	return &WorkService{store: store, userCfg: userCfg, appClient: appClient}
 }
 
-// Helper function for getting a student work by ID
-func (s *WorkService) getWork(c *fiber.Ctx) (*models.PaginatedStudentWorkWithContributors, error) {
-	classroomID, err := strconv.Atoi(c.Params("classroom_id"))
-	if err != nil {
-		return nil, errs.BadRequest(err)
-	}
-	assignmentID, err := strconv.Atoi(c.Params("assignment_id"))
-	if err != nil {
-		return nil, errs.BadRequest(err)
-	}
-	studentWorkID, err := strconv.Atoi(c.Params("work_id"))
-	if err != nil {
-		return nil, errs.BadRequest(err)
-	}
+// Getter for store field
+func (s *WorkService) GetStore() storage.Storage {
+	return s.store
+}
 
-	work, err := s.store.GetWork(c.Context(), classroomID, assignmentID, studentWorkID)
-	if err != nil {
-		return nil, errs.NotFoundMultiple("student work", map[string]string{
-			"classroom ID":          c.Params("classroom_id"),
-			"assignment outline ID": c.Params("assignment_id"),
-			"student work ID":       c.Params("work_id"),
-		})
-	}
+// Getter for userCfg field
+func (s *WorkService) GetUserCfg() *config.GitHubUserClient {
+	return s.userCfg
+}
 
-	return work, nil
+// Getter for appClient field
+func (s *WorkService) GetAppClient() github.GitHubAppClient {
+	return s.appClient
 }
