a912e3739ee02d50b3ec644f65b882494679d85a
kennybc <54950614+kennybc@users.noreply.github.com>
2024-12-06 16:57:47 -0500

    rm dupe block

--- backend/internal/github/github.go
+++ backend/internal/github/github.go
@@ -57,11 +57,6 @@ type GitHubUserClient interface { // All methods in the OAUTH client
 
 	// Create initial feedback pull request
 	CreateFeedbackPR(ctx context.Context, owner, repo string) error
-	// Fork a repository as a student
-	ForkRepository(ctx context.Context, srcOwner, srcRepo, dstOrg, dstRepo string) error
-
-	// Create initial feedback pull request
-	CreateFeedbackPR(ctx context.Context, owner, repo string) error
 }
 
 type GitHubBaseClient interface { //All methods in the SHARED client
