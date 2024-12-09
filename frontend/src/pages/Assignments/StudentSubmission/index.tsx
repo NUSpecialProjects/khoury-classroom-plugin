6f4528354ca00963fd71a60191ba79dad03ec390
nandini-ghosh <nandini.ghosh2094@gmail.com>
2024-12-09 15:29:08 -0500

    metrics added

--- frontend/src/pages/Assignments/StudentSubmission/index.tsx
+++ frontend/src/pages/Assignments/StudentSubmission/index.tsx
@@ -13,55 +13,62 @@ import { MdEdit } from "react-icons/md";
 import { FaGithub } from "react-icons/fa";
 
 const StudentSubmission: React.FC = () => {
-    const location = useLocation();
-    const [submission, setSubmission] = useState<IStudentWork>();
-    const { id } = useParams();
-    const { selectedClassroom } = useContext(SelectedClassroomContext);
-    const assignmentID = location.state.assignmentId;
+  const location = useLocation();
+  const [submission, setSubmission] = useState<IStudentWork>();
+  const { id } = useParams();
+  const { selectedClassroom } = useContext(SelectedClassroomContext);
+  const assignmentID = location.state.assignmentId;
 
-    console.log(location.state);
+  console.log(location.state);
 
-    useEffect(() => {
-        if (location.state && location.state.submission) {
-          setSubmission(location.state.submission); // Use submission from state
-        } else if (id && assignmentID && selectedClassroom) {
-          // If state fails, use submission data as a fallback
-          (async () => {
-            try {
-              const fetchedSubmission = await getStudentWorkById(
-                selectedClassroom.id,
-                assignmentID, // Use assignmentID from state
-                +id // Student submission ID
-              );
-              if (fetchedSubmission) {
-                setSubmission(fetchedSubmission);
-              }
-            } catch (error) {
-              console.error("Failed to fetch submission:", error);
-            }
-          })();
+  useEffect(() => {
+    if (location.state && location.state.submission) {
+      setSubmission(location.state.submission); // Use submission from state
+    } else if (id && assignmentID && selectedClassroom) {
+      // If state fails, use submission data as a fallback
+      (async () => {
+        try {
+          const fetchedSubmission = await getStudentWorkById(
+            selectedClassroom.id,
+            assignmentID, // Use assignmentID from state
+            +id // Student submission ID
+          );
+          if (fetchedSubmission) {
+            setSubmission(fetchedSubmission);
+          }
+        } catch (error) {
+          console.error("Failed to fetch submission:", error);
         }
-      }, [location.state, id, selectedClassroom]);
-    
-    return (
-        <div className="StudentWork">
-            <SubPageHeader
-                pageTitle={submission?.contributors.join(", ")}
-                pageSubTitle={submission?.assignment_name}
-                chevronLink={`/app/assignments/${assignmentID}`}
-            >
-            </SubPageHeader>
+      })();
+    }
+  }, [location.state, id, selectedClassroom]);
 
-            <div className="StudentSubmission__externalButtons">
-            <Button href="#" variant="secondary" newTab>
-              <FaGithub className="icon" /> View Student Repository
-            </Button>
-            <Button href="#" variant="secondary" newTab>
-              <MdEditDocument className="icon" />  Grade Submission
-            </Button>
-          </div>
-        </div>
-    );
+  return (
+    <div className="StudentWork">
+      <SubPageHeader
+        pageTitle={submission?.contributors.join(", ")}
+        pageSubTitle={submission?.assignment_name}
+        chevronLink={`/app/assignments/${assignmentID}`}
+      >
+      </SubPageHeader>
+
+      <div className="StudentSubmission__externalButtons">
+        <Button href="#" variant="secondary" newTab>
+          <FaGithub className="icon" /> View Student Repository
+        </Button>
+        <Button href="#" variant="secondary" newTab>
+          <MdEditDocument className="icon" />  Grade Submission
+        </Button>
+      </div>
+
+      <div className="StudentSubmission__subSectionWrapper">
+        <h2 style={{ marginBottom: 10 }}>Metrics</h2>
+        <MetricPanel>
+          <SimpleMetric metricTitle="First Commit Date" metricValue="Put Chart Here"></SimpleMetric>
+        </MetricPanel>
+      </div>
+    </div>
+  );
 };
 
 export default StudentSubmission;
