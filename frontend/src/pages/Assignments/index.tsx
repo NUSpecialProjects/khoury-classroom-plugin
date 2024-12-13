913542294e862b34f9ca7dc3bbbc1ef341c6954a
kennybc <54950614+kennybc@users.noreply.github.com>
2024-12-13 15:22:40 -0500

    add breadcrumb to all main pages, move spinner to layout

--- frontend/src/pages/Assignments/index.tsx
+++ frontend/src/pages/Assignments/index.tsx
@@ -1,10 +1,23 @@
+import { useContext } from "react";
+
+import { SelectedClassroomContext } from "@/contexts/selectedClassroom";
+
+import BreadcrumbPageHeader from "@/components/PageHeader/BreadcrumbPageHeader";
 import { GradeDistBoxPlot } from "@/components/Viz/BoxPlot/GradeDistBoxPlot";
 
 const Assignments: React.FC = () => {
+  const { selectedClassroom } = useContext(SelectedClassroomContext);
+
   return (
-    <>
-      <GradeDistBoxPlot />
-    </>
+    selectedClassroom && (
+      <>
+        <BreadcrumbPageHeader
+          pageTitle={selectedClassroom?.org_name}
+          breadcrumbItems={[selectedClassroom?.name, "Assignments"]}
+        />
+        <GradeDistBoxPlot />
+      </>
+    )
   );
 };
 
