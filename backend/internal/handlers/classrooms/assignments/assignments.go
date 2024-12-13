91d6c7d1353cd67cf1b9e3d611f420680a93764a
Alex Angione <111721125+alexangione419@users.noreply.github.com>
2024-12-13 15:46:15 -0500

    format

--- backend/internal/handlers/classrooms/assignments/assignments.go
+++ backend/internal/handlers/classrooms/assignments/assignments.go
@@ -342,7 +342,7 @@ func (s *AssignmentService) getAssignmentRubric() fiber.Handler {
 
 		if assignment.RubricID == nil {
 			return c.Status(http.StatusOK).JSON(nil)
-        }
+		}
 
 		rubric, err := s.store.GetRubric(c.Context(), *assignment.RubricID)
 		if err != nil {
