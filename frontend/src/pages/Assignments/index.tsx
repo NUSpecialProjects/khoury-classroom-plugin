1b204d584430a74f503a99007f732505229e9843
kennybc <54950614+kennybc@users.noreply.github.com>
2024-12-17 11:10:48 -0500

    rm unused

--- frontend/src/pages/Assignments/index.tsx
+++ frontend/src/pages/Assignments/index.tsx
@@ -1,39 +1,12 @@
-import { useContext, useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import { MdAdd } from "react-icons/md";
+import { useContext } from "react";
 
 import { SelectedClassroomContext } from "@/contexts/selectedClassroom";
-import { getAssignments } from "@/api/assignments";
-import { formatDateTime } from "@/utils/date";
 
 import BreadcrumbPageHeader from "@/components/PageHeader/BreadcrumbPageHeader";
 import { GradeDistBoxPlot } from "@/components/Viz/BoxPlot/GradeDistBoxPlot";
-import { Table, TableRow, TableCell } from "@/components/Table";
-import Button from "@/components/Button";
 
 const Assignments: React.FC = () => {
   const { selectedClassroom } = useContext(SelectedClassroomContext);
-  const [assignments, setAssignments] = useState<IAssignmentOutline[]>([]);
-
-  useEffect(() => {
-    const fetchAssignments = async (classroom: IClassroom) => {
-      if (classroom) {
-        getAssignments(classroom.id)
-          .then((assignments) => {
-            setAssignments(assignments);
-          })
-          .catch((_: unknown) => {
-            // do nothing
-          });
-      }
-    };
-
-    if (selectedClassroom !== null && selectedClassroom !== undefined) {
-      fetchAssignments(selectedClassroom).catch((_: unknown) => {
-        // do nothing
-      });
-    }
-  }, [selectedClassroom]);
 
   return (
     selectedClassroom && (
